e12abfdf7f0d3fa0cf4dac2e33fe9382a2b183cf
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-12-27 23:11:00 -0800

    feat: add api for flask routes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/p/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5328/api/p/:path*"
+            : "/api/p/",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
