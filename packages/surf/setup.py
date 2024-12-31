4cfc36b4153741d8b5c5ae62f468d2846cb25bde
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-12-30 21:40:24 -0800

    updates

---
+++ packages/surf/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="surf",
+    version="0.1.0",
+    packages=find_namespace_packages(include=["surf", "surf.*"]),
+    package_dir={"": "api"},
+    install_requires=[
+        "pytz",
+        "netCDF4",
+        "requests",
+    ],
+) 
